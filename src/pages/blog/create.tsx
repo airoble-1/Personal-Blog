415abc0d889ef5a2007e440e9f3e20709d7dc9f2
airoble-1 <airoble2019@gmail.com>
2022-11-27 16:49:55 -0500

    Add form validation  & fix create blog api route

--- src/pages/blog/create.tsx
+++ src/pages/blog/create.tsx
@@ -1,25 +1,38 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { SyntheticEvent, useState } from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+type Formvalues = {
+  name: string;
+  description: string;
+  file: File;
+};
 
 const Home: NextPage = () => {
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
+  // const [nameValue, setName] = useState("");
+  // const [description, setDescription] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [selectedFIle, setSelectedFile] = useState<File>();
+  // const [selectedFIle, setSelectedFile] = useState();
   const router = useRouter();
 
-  const handleImageChange = function (e: React.ChangeEvent<HTMLInputElement>) {
-    const fileList = e.target.files;
-    if (!fileList) return;
-    setSelectedFile(fileList[0]);
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Formvalues>();
+  console.log(errors);
+  // const handleImageChange = function (data) {
+  //   const fileList = e.target.files;
+  //   if (!fileList) return;
+  //   setSelectedFile(fileList[0]);
+  // };
 
-  const uploadFile = async function () {
-    if (selectedFIle) {
+  const uploadFile = async function (data) {
+    if (data.file[0]) {
       const formData = new FormData();
-      formData.append("file", selectedFIle);
+      formData.append("file", data.file[0]);
       formData.append("upload_preset", "my-uploads");
       const response = await fetch(
         "https://api.cloudinary.com/v1_1/dlwqjptsg/image/upload",
@@ -31,11 +44,12 @@ const Home: NextPage = () => {
       return response.json();
     }
   };
-  async function handleSubmit(e: SyntheticEvent) {
-    e.preventDefault();
+  async function submitHandler(userData) {
+    // e.preventDefault();
+    const { name, description } = userData;
     setIsLoading(true);
     try {
-      const fileData = await uploadFile();
+      const fileData = await uploadFile(userData);
       console.log("upload data: ", fileData);
       if (!fileData) return;
       const response = await fetch(`${window.location.origin}/api/blog`, {
@@ -65,16 +79,32 @@ const Home: NextPage = () => {
 
           <h3 className="fw-bolder">Blog</h3>
           <hr />
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit((data) => submitHandler(data))}>
             <div className="text-danger"></div>
             <div className="form-group mb-3">
               <label className="control-label fw-bold fs-5 mb-2">Name</label>
               <input
                 type="text"
+                {...register("name", {
+                  required: "Blog name is required",
+                  minLength: {
+                    value: 2,
+                    message:
+                      "The name must be at least 2 to 100 characters long.",
+                  },
+                  maxLength: {
+                    value: 100,
+                    message:
+                      "The name must be at least 2 to 100 characters long.",
+                  },
+                })}
                 className="form-control"
-                onChange={(e) => setName(e.target.value)}
-                value={name}
+                //onChange={(e) => setName(e.target.value)}
+                //name={name}
               />
+              {errors.name && (
+                <div className="text-danger">{errors.name.message}</div>
+              )}
               <span className="text-danger"></span>
             </div>
             <div className="form-group mb-3">
@@ -82,14 +112,26 @@ const Home: NextPage = () => {
                 Description
               </label>
               <textarea
-                onChange={(e) => setDescription(e.target.value)}
-                value={description}
+                {...register("description", {
+                  required: "Blog description is required",
+                  minLength: {
+                    value: 2,
+                    message:
+                      "The description must be at least 2 to 500 characters long.",
+                  },
+                  maxLength: {
+                    value: 500,
+                    message:
+                      "The description must be at least 2 to 500 characters long.",
+                  },
+                })}
+                //   onChange={(e) => setDescription(e.target.value)}
+                // value={description}
                 className="form-control"
               ></textarea>
-              <span
-                asp-validation-for="Description"
-                className="text-danger"
-              ></span>
+              {errors.description && (
+                <div className="text-danger">{errors.description.message}</div>
+              )}
             </div>
 
             <div className="form-group">
@@ -100,13 +142,17 @@ const Home: NextPage = () => {
                 Select Image
               </label>
               <input
-                name="file"
+                {...register("file", {
+                  required: "Blog feature image is required",
+                })}
                 type="file"
                 className="form-control"
                 accept=".jpg,.png,.gif,.jpeg,.svg"
-                onChange={(e) => handleImageChange(e)}
+                // onChange={(e) => handleImageChange(e)}
               />
-              <span className="text-danger"></span>
+              {errors.file && (
+                <div className="text-danger">{errors.file.message}</div>
+              )}
             </div>
 
             <div className="form-group mt-3 mb-3">
